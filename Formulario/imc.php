73bda091c5d9a193b1d47e70695128fd5df8cbba
gabryelsoliveira <gabryels.oliveira08@gmail.com>
2026-02-24 17:20:53 -0300

    Create imc.php

---
+++ Formulario/imc.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$nome = $_GET['nome'];
+$peso = $_GET['peso'];    
+$altura = $_GET['altura'];
+$imc = $peso / ($altura * $altura);
+
+echo 'Olá ' . $nome . ' imc é de : ' . round($imc, 2) . '<br>';
+
+if($imc < 18.5){
+    echo 'Você está abaixo do peso!';
+}else if ($imc == 18.5 || $imc < 24.9){
+    echo 'Você está com peso normal!';
+} else { 
+    echo 'Você está acima do peso!';
+}
+
+
+
+
+?>
+</body>
+</html>
