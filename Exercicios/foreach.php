5e7a3767be527cd3c0bf93e39f4ce1f4568df59f
gabryelsoliveira <gabryels.oliveira08@gmail.com>
2026-02-24 17:20:44 -0300

    Create foreach.php

---
+++ Exercicios/foreach.php
@@ -0,0 +1,8 @@
+<?php                    
+//Foreach                    
+foreach($frutas as $fruta){             
+echo $fruta . '<br>';                    
+        //"$fruta <br>"                    
+}                    
+
+?>
