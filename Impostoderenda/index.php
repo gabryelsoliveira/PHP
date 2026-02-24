022642162d9c0f7d1aa1684ad380957d1b37770c
gabryelsoliveira <gabryels.oliveira08@gmail.com>
2026-02-24 17:21:19 -0300

    Create index.php

---
+++ Impostoderenda/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="calculo.php" method="get">
+    <label for="salario">Salário Bruto:</label>
+    <input type="number" id="salario" name="salario" step="0.01">
+
+    <input type="submit" value="Calcular Imposto de Renda">
+
+
+</form>
+
+
+
+</body>
+</html>
