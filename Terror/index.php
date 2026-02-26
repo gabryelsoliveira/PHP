6d2809602bd75e8b021206a1ca4633900515084e
gabryelsoliveira <gabryels.oliveira08@gmail.com>
2026-02-26 14:22:54 -0300

    Create index.php

---
+++ Terror/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Personagem.php" method="post">
+   <select name="personagens">
+  <option value="1">Jason</option>
+  <option value="2">Freddy Krueger</option>
+  <option value="3">Chuck</option>
+  <option value="4">Pennywise(It)</option>
+  <option value="5">Ghostface</option>
+  <option value="6">Art the Clown</option>
+</select>
+</form>
+
+<br>
+
+<button onclick="Apertar()">Ver Sobre...</button>
+
+</body>
+<html>
